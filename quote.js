f1c2b08318c24920d88cca6b9f4a316692ebe3a9
Sujal-Polawala <sujalpolawala@gmail.com>
2025-04-03 19:42:06 +0530

    Add Instagram and Snapchat Share

--- quote.js
+++ quote.js
@@ -4,6 +4,8 @@ const quoteText = document.querySelector(".quote"),
   speechBtn = document.querySelector(".speech"),
   copyBtn = document.querySelector(".copy"),
   twitterBtn = document.querySelector(".twitter"),
+  snapchatBtn = document.querySelector(".snapchat"), // Added Snapchat
+  instagramBtn = document.querySelector(".instagram"), // Added Instagram
   synth = speechSynthesis;
 
 function randomQuote() {
@@ -35,6 +37,7 @@ function randomQuote() {
 // Event listeners
 quoteBtn.addEventListener("click", randomQuote);
 
+// Speech feature
 speechBtn.addEventListener("click", () => {
   if (!quoteBtn.classList.contains("loading")) {
     let utterance = new SpeechSynthesisUtterance(
@@ -49,13 +52,35 @@ speechBtn.addEventListener("click", () => {
   }
 });
 
+// Copy feature
 copyBtn.addEventListener("click", () => {
   navigator.clipboard.writeText(quoteText.innerText);
 });
 
+// Twitter Share
 twitterBtn.addEventListener("click", () => {
-  let tweetUrl = `https://twitter.com/intent/tweet?url=${encodeURIComponent(
-    quoteText.innerText
+  let tweetUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(
+    quoteText.innerText + " - " + authorName.innerText
   )}`;
   window.open(tweetUrl, "_blank");
+});
+
+// Snapchat Share
+snapchatBtn.addEventListener("click", () => {
+  let quote = `${quoteText.innerText} - ${authorName.innerText}`;
+  let snapLensUrl = `https://lens.snapchat.com/b2da6f0c7fc847e38ee263a2063b86e1?share_id=2NMkwpbQ22A&locale=en-IN&metadata=${encodeURIComponent(quote)}`;
+
+  // Open Snapchat Lens with the quote
+  window.open(snapLensUrl, "_blank");
+});
+
+// Instagram Share (works on mobile only)
+instagramBtn.addEventListener("click", () => {
+  let quote = `${quoteText.innerText} - ${authorName.innerText}`;
+
+  // Open Instagram Story with text
+  let instagramUrl = `instagram://story?source_application=com.yourappname&text=${encodeURIComponent(quote)}`;
+
+  // Open the Instagram app
+  window.location.href = instagramUrl;
 });
